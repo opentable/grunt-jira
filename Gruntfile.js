fb369ebf6e01313b942e69efe29ec49fc3ab2728
Doron Segal <doron2402@gmail.com>
2015-06-01 11:28:31 -0700

    latest changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,54 @@
 module.exports = function(grunt) {
-    'use strict';
+  'use strict';
 
-    var pkg = grunt.file.readJSON('package.json');
+  var pkg = grunt.file.readJSON('package.json');
 
-    // Project configuration.
-    grunt.initConfig({
+  // Project configuration.
+  grunt.initConfig({
 
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/*.js'
-            ],
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
-        mochaTest:{
-            options: {
-                reporter: 'spec'
-            },
-            tests:{
-                src: ['tests/ccb-tests.js']
-            }
-        },
-        "ccb": {
-            test: {
-                options: {
-                    jira: {
-                        host: "https://localhost:9000",
-                        username: "usertest",
-                        password: "passwordtest",
-                        project_id: "4321",
-                        ccb_issue_type: 20,
-                        ccb_done_state: 11
-                    },
-                    project: {
-                      name: "projectname",
-                    },
-                    manifest: "tests/data/manifest.json",
-                    build_number: "grunt-ccb_1234"
-                }
-            }
+    jshint: {
+        all: [
+            'Gruntfile.js',
+            'tasks/*.js'
+        ],
+        options: {
+            jshintrc: '.jshintrc'
         }
-    });
+    },
+    mochaTest:{
+      options: {
+        reporter: 'spec'
+      },
+      tests:{
+        src: ['tests/ccb-tests.js']
+      }
+    },
+    "ccb": {
+      test: {
+        options: {
+          jira: {
+              host: "https://localhost:9000",
+              username: "usertest",
+              password: "passwordtest",
+              project_id: "4321",
+              ccb_issue_type: 20,
+              ccb_done_state: 11
+          },
+          project: {
+            name: "projectname",
+          },
+          manifest: "tests/data/manifest.json",
+          build_number: "grunt-ccb_1234"
+        }
+      }
+    }
+  });
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.registerTask('test', ['jshint',  'start-jira-server', 'ccb:test', 'mochaTest']);
-    grunt.registerTask('default', ['test']);
-    grunt.loadTasks('tasks');
-    grunt.loadTasks('tests/tasks');
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.registerTask('test', ['jshint',  'start-jira-server', 'ccb:test', 'mochaTest']);
+  grunt.registerTask('default', ['test']);
+  grunt.loadTasks('tasks');
+  grunt.loadTasks('tests/tasks');
 };
--- tests/tasks/dummyJiraServer.js
+++ tests/tasks/dummyJiraServer.js
@@ -5,10 +5,10 @@ var options = {};
 var port = 9000;
 
 try {
-    options = {
-      key: fs.readFileSync('tests/keys/key.pem'),
-      cert: fs.readFileSync('tests/keys/key-cert.pem')
-    };
+  options = {
+    key: fs.readFileSync('tests/keys/key.pem'),
+    cert: fs.readFileSync('tests/keys/key-cert.pem')
+  };
 
   fs.mkdirSync("tests/data/actual");
 }
@@ -18,37 +18,36 @@ var createCbbRequest = fs.openSync('tests/data/actual/ccb-request.json', 'w');
 var doneTransitionRequest = fs.openSync('tests/data/actual/done-transition-request.json', 'w');
 
 module.exports = function(grunt){
-    grunt.registerTask('start-jira-server', function(){
-        server = https.createServer(options, function(request, response) {
-            console.log(request);
-            if (request.method != 'POST'){
-                throw new Error("This dummy server only responds to POST requests");
-            }
+  grunt.registerTask('start-jira-server', function(){
+    server = https.createServer(options, function(request, response) {
+      console.log('request');
+      console.log(request);
+      if (request.method != 'POST'){
+        throw new Error("This dummy server only responds to POST requests");
+      }
 
-            var body = '';
-            request.on('data', function (data) {
-                body += data;
-            });
+      var body = '';
+      request.on('data', function (data) {
+          body += data;
+      });
 
-            request.on('end', function () {
+      request.on('end', function () {
 
-                var bodyJson = JSON.parse(body);
+        var bodyJson = JSON.parse(body);
 
-                if (request.url === '/issue/') //create ccb
-                {
-                    fs.writeSync(createCbbRequest, JSON.stringify({ headers: request.headers, url: request.url, body: bodyJson  }));
-                    response.writeHead(200, {"Content-Type": "application/json"});
-                    response.write('{ "id": 1234 }');
-                }
-                else if (bodyJson.transition.id === 11){ //done
+        if (request.url === '/issue/') //create ccb
+        {
+          fs.writeSync(createCbbRequest, JSON.stringify({ headers: request.headers, url: request.url, body: bodyJson  }));
+          response.writeHead(200, {"Content-Type": "application/json"});
+          response.write('{ "id": 1234 }');
+        }
+        else if (bodyJson.transition.id === 11){ //done
+          fs.writeSync(doneTransitionRequest, JSON.stringify({ headers: request.headers, url: request.url, body: bodyJson }));
+          response.writeHead(200, {"Content-Type": "application/json"});
+        }
 
-                    fs.writeSync(doneTransitionRequest, JSON.stringify({ headers: request.headers, url: request.url, body: bodyJson }));
-                    response.writeHead(200, {"Content-Type": "application/json"});
-
-                }
-
-                response.end();
-            });
-        }).listen(port);
-    });
+        response.end();
+      });
+    }).listen(port);
+  });
 };
